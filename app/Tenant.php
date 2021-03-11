b1383e2a87228c0bb77e650574dcebbbc69be8b9
xurror <kaze.nasser@outlook.com>
2021-03-11 07:57:39 +0100

    tenant model

---
+++ app/Tenant.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+
+class Tenant extends Model
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'slug',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        //
+    ];
+
+    public function route($name, $parameters = []) {
+        return 'https://' . $this->slug . app('url')->route($name, $parameters, false);
+    }
+}
