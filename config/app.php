909e35e2e2b6338eb11f76f642fa1f793482a638
xurror <kaze.nasser@outlook.com>
2021-03-14 14:27:28 +0100

    Fetch app domain from env props

--- config/app.php
+++ config/app.php
@@ -14,6 +14,7 @@ return [
     */
 
     'name' => env('APP_NAME', 'Laravel'),
+    'domain' => env('TENANT_DOMAIN', 'localhost'),
 
     /*
     |--------------------------------------------------------------------------
