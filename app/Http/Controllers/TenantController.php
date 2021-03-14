7772334bfda33f54f2e21e27f6eeb6eb492c0fa4
xurror <kaze.nasser@outlook.com>
2021-03-14 14:30:38 +0100

    Add method to register a new tenant

--- app/Http/Controllers/TenantController.php
+++ app/Http/Controllers/TenantController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use App\Services\TenantManager;
+use App\Models\Tenant;
+use Illuminate\Http\Request;
+use App\Services\SlugService;
+use App\Jobs\SetupTenantDatabaseJob;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -13,6 +16,13 @@ class TenantController extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
+    protected $slugService;
+
+    public function __construct(SlugService $slugService)
+    {
+        $this->slugService = $slugService;
+    }
+
     /**
      * Show the profile for the given user.
      *
@@ -21,11 +31,53 @@ class TenantController extends BaseController
      */
     public function show()
     {
-        $users = User::all();
-        return view('welcome');
-        // return view('welcome', [
-        //     'slug' => $manager->getTenant()->slug,
-        //     'users' => $users
-        // ]);
+        $users = null;
+        $slug = $this->slugService->getSlug();
+        $tenant = Tenant::where('slug', '=', $slug)->first();
+
+        if (is_null($slug))
+        {
+            return view('welcome');
+        }
+        elseif (!is_null($slug) && is_null($tenant))
+        {
+            return view('welcome', ['notfound' => true]);
+        }
+        else
+        {
+            $users = User::all();
+            return view('welcome', ['slug' => $slug, 'users' => $users]);
+        }
+
+    }
+
+    /**
+     * Show the profile for the given user.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function create(Request $request) {
+        $this->validate($request, [
+            'name' => 'required',
+            'email' => 'required|email',
+            'slug' => 'required',
+        ]);
+
+        $slug = $request->slug;
+
+        $tenant = Tenant::where('slug', '=', $slug)->first();
+
+        if ($tenant == null)
+        {
+            $tenant = new Tenant();
+            $tenant->slug = $slug;
+            $tenant->name = $request->name;
+            $tenant->email = $request->email;
+            $tenant->save();
+
+            dispatch(new SetupTenantDatabaseJob($tenant));
+        }
+        return redirect('/');
     }
 }
